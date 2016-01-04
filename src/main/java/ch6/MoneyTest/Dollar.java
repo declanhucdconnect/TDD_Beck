128fcdb73c71e8d50cdeb89c4cac2e015c6c105d
Declan Hynes <declan.hynes+github@gmail.com>
2016-01-04 21:41:00 +0000

    A start on Ch6.

--- src/main/java/ch6/MoneyTest/Dollar.java
+++ src/main/java/ch6/MoneyTest/Dollar.java
@@ -1,20 +1,25 @@
 package ch6.MoneyTest;
 
-/**
- * Created by declanh on 1/3/2016.
- */
-public class Money {
-    // private int amount;
 
-    protected int amount;
+public class Dollar extends Money{
+
+    Dollar times(int multiplier){
+        return new Dollar(amount * multiplier);
+    }
+
+    Dollar(int dollar){
+        this.amount = dollar;
+    }
 
     /**
      * Compares the amount of the current object, to the amount of the Dollar object that is passed in as a parameter.
      * @param object - the object to compare the current "dollar" object to.
      * @return true if the amounts are the same.
      */
+    /*
     public boolean equals(Object object){
         Money money = (Money) object;
         return amount == money.amount;
     }
+    */
 }
--- src/main/java/ch6/MoneyTest/Franc.java
+++ src/main/java/ch6/MoneyTest/Franc.java
@@ -1,20 +1,25 @@
 package ch6.MoneyTest;
 
-/**
- * Created by declanh on 1/3/2016.
- */
-public class Money {
-    // private int amount;
+public class Franc extends Money{
 
-    protected int amount;
+
+    Franc times(int multiplier){
+        return new Franc(amount * multiplier);
+    }
+
+    Franc(int franc){
+        this.amount = franc;
+    }
 
     /**
      * Compares the amount of the current object, to the amount of the Dollar object that is passed in as a parameter.
      * @param object - the object to compare the current "dollar" object to.
      * @return true if the amounts are the same.
      */
+    /*
     public boolean equals(Object object){
         Money money = (Money) object;
         return amount == money.amount;
     }
+    */
 }
