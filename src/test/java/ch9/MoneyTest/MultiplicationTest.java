b8ce18ea0c76bd3335961c03635d01af5ec7a61e
Declan Hynes <declan.hynes+github@gmail.com>
2016-01-05 21:27:11 +0000

    CH 9: Looking into the concept of currencies. Created a simple test - to test against strings. Started to implement strings for each of the currencies, then pulled it up into the Money class. During this refactoring (in small steps)  - pass the variable, but not actually doing the assignment - and then noticed that there was a times() method - within Franc that was calling a constructor rather than a factory method...and we allowed a small interruption to be permitted to fix it - but only a single small interruption. Did the same refactoring in Dollar.

---
+++ src/test/java/ch9/MoneyTest/MultiplicationTest.java
@@ -0,0 +1,76 @@
+package ch9.MoneyTest;
+
+/**
+ * Created by declanh on 12/28/2015.
+ *
+ * TODO:
+ *  $5 + 10 CHF = $10 if rate is 2:1
+ *  $5 * 2 = $10 <<-- XX
+ *  Make "amount" private <<-- XX
+ *  Dollar side effects? <<-- XX
+ *  Money Rounding?
+ *  equals()<<-- XX
+ *  hashCode()
+ *  Equal null
+ *  Equal object
+ *  5 CHF * 2 = 10 CHF  <<-- XX
+ *  Dollar/Franc duplication  (Not quite completed yet! - dh)
+ *  Common equals <<-- XX
+ *  Common times
+ *  Compare Francs with Dollars
+ *  Currency ? <<-- XX Focus of Chapter 9 - Times We're Livin' In
+ *  Delete testFrancMultiplication ?
+ */
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by declanh on 12/28/2015.
+ */
+public class MultiplicationTest {
+
+    @org.junit.Before
+    public void setUp() throws Exception {
+
+    }
+
+    @org.junit.After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testMultiplication(){
+        Money five = Money.dollar(5);
+        assertEquals(Money.dollar(10), five.times(2));
+        assertEquals(Money.dollar(15), five.times(3));
+    }
+
+
+    @Test
+    public void testFrancMultiplication(){
+        Money five = Money.franc(5);
+        assertEquals(Money.franc(10), five.times(2));
+        assertEquals(Money.franc(15), five.times(3));
+    }
+
+    @Test
+    public void testEquality(){
+        assertTrue(Money.dollar(5).equals( Money.dollar(5)));
+        assertFalse((Money.dollar(5).equals(Money.dollar(6))));
+
+        assertTrue( Money.franc(5).equals(Money.franc(5)));
+        assertFalse((Money.franc(5).equals(Money.franc(6))));
+
+        assertFalse((Money.franc(5).equals(Money.dollar(5))));
+    }
+
+    @Test
+    public void testCurrency(){
+        assertEquals("USD", Money.dollar(1).currency());
+        assertEquals("CHF", Money.franc(1).currency());
+
+    }
+}
