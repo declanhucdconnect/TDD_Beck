498b0c33aa91614fac0858eb1f135a6d00d7603b
Declan Hynes <declan.hynes+github@gmail.com>
2016-01-05 20:49:50 +0000

    CH8 - Makin' Objects Here we examine Dollar and Franc duplication. Some thoughtful refactoring removes the dependency on the subclasses by introducing some factory methods. Primarily because Dollar and Franc were not doing enough independent work to be useful.
    The thought process was:
    * Fewer references to subclasses -so a factory method
    * Create relevant tests using those methods
    * Make Money abstract as it is a little too early (in the steps in the book) to implement times within Money - so times in Money is abstract, and still concrete within Dollar and Franc.
    
    There are some possible redundant tests (testFrancMultiplication - but a sliver of confidence may be lost in the code if we remove it - so we leave it there for now..... for the future!

---
+++ src/main/java/ch8/MoneyTest/Franc.java
@@ -0,0 +1,25 @@
+package ch8.MoneyTest;
+
+public class Franc extends Money {
+
+
+    Money times(int multiplier){
+        return new Franc(amount * multiplier);
+    }
+
+    Franc(int franc){
+        this.amount = franc;
+    }
+
+    /**
+     * Compares the amount of the current object, to the amount of the Dollar object that is passed in as a parameter.
+     * @param object - the object to compare the current "dollar" object to.
+     * @return true if the amounts are the same.
+     */
+    /*
+    public boolean equals(Object object){
+        Money money = (Money) object;
+        return amount == money.amount;
+    }
+    */
+}
